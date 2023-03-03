f3274db15a665fb5fd43d7e295cea4b2db2580ea
Harlok <holmik90@yandex.ru>
2023-03-03 13:22:51 +0300

    feat(./command_handler.py): add MENU_BOARD for send_welcome

--- lariska_bot/handlers/command_handler.py
+++ lariska_bot/handlers/command_handler.py
@@ -1,9 +1,10 @@
 from aiogram.types import Message
+from aiogram import Router
+from aiogram.filters import Command
 
 from lariska_bot.handlers.handlers_data.commands import *
 from lariska_bot.handlers.handlers_data.messages import *
-from aiogram import Router
-from aiogram.filters import Command
+from lariska_bot.keyboards.reply_keyboard import *
 
 router = Router()
 
@@ -18,7 +19,9 @@ async def youtube_answer(message: Message):
 
 async def send_welcome(message: Message):
     await message.delete()
-    await message.answer(get_welcome(), parse_mode='MARKDOWN')
+    await message.answer(get_welcome(),
+                         parse_mode='MARKDOWN',
+                         reply_markup=MENU_BOARD)
 
 
 def register_command_handler(router: Router):
