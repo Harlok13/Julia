af7d1a67d04a3f497e663cd0d957984b95941201
Harlok <holmik90@yandex.ru>
2023-03-04 18:13:34 +0300

    feat(./keyboards/set_menu.py): add set_main_menu

---
+++ lariska_bot/keyboards/set_menu.py
@@ -0,0 +1,15 @@
+from typing import List
+
+from aiogram import Bot
+from aiogram.types import BotCommand
+
+from lariska_bot.handlers.handlers_data.commands import COMMANDS
+
+
+async def set_main_menu(bot: Bot) -> None:
+    """Создание Menu с командами."""
+    main_menu_commands: List[BotCommand] = [BotCommand(
+        command=command,
+        description=description
+    ) for command, description in COMMANDS.items()]
+    await bot.set_my_commands(main_menu_commands)
