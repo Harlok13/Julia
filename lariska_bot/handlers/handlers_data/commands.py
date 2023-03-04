5f552f328ace060b2cb414f62d7d284784351da0
Harlok <holmik90@yandex.ru>
2023-03-04 22:33:38 +0300

    feat(./commands.py): add dict COMMANDS
    add commands
    /start
    /library
    /help
    /repo
    /youtube

--- lariska_bot/handlers/handlers_data/commands.py
+++ lariska_bot/handlers/handlers_data/commands.py
@@ -1,4 +1,16 @@
-def get_repo_list():
+from typing import Dict, List
+
+COMMANDS: Dict[str, str] = {
+    '/start': 'на время тестирования, затем будет удалена',
+    '/library': 'наша сокровищница книга',
+    '/help': 'справка и помощь',
+    '/repo': 'наш репозиторий',
+    '/youtube': 'ссылка на ютуб главдеда'
+}
+
+
+def get_repo_list() -> List[str]:
+    """Фильтр команды для получения ссылки на репозиторий."""
     return [
         'repo',
         'repository',
@@ -7,9 +19,15 @@ def get_repo_list():
     ]
 
 
-def get_video_list():
+def get_video_list() -> List[str]:
+    """Фильтр команды для получения ссылки на ютуб."""
     return [
         'ютуб',
         'youtube',
         'video',
     ]
+
+
+def get_library() -> str:
+    """Фильтр команды для открытия меню библиотеки."""
+    return 'library'
