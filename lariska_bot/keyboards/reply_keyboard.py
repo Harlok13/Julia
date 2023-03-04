e272472fd8914211ef138831065d6ed50a80d2c8
Harlok <holmik90@yandex.ru>
2023-03-04 22:26:08 +0300

    feat(./keyboards/reply_keyboard.py): add new button

--- lariska_bot/keyboards/reply_keyboard.py
+++ lariska_bot/keyboards/reply_keyboard.py
@@ -1,10 +1,12 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, WebAppInfo
 
-MENU_BOARD = ReplyKeyboardMarkup(
+MENU_BOARD: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
     resize_keyboard=True,
     one_time_keyboard=True,
+    input_field_placeholder='выберите действие',
     keyboard=[
         [KeyboardButton(text='БИБЛИОТЕКА')],
+        [KeyboardButton(text='НАША РЕПА', web_app=WebAppInfo(url='https://github.com/OldCodersClub/faq'))],
 
     ]
 )
