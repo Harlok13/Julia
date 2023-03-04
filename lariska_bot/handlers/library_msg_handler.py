cb8fdd9233787862967c1561c0656b7bc9ba7a62
Harlok <holmik90@yandex.ru>
2023-03-04 22:38:15 +0300

    ref(all): ref data, add type hinting

--- lariska_bot/handlers/library_msg_handler.py
+++ lariska_bot/handlers/library_msg_handler.py
@@ -4,11 +4,12 @@ from aiogram.types import Message
 from lariska_bot.keyboards.lybrary_inline_kb import CAT_CHOICE_MENU
 
 
-async def get_library(message: Message):
+async def get_library(message: Message) -> None:
+    """Открыть меню библиотеки."""
+    await message.delete()
     await message.answer(text='Выберите категорию:',
                          reply_markup=CAT_CHOICE_MENU)
-    await message.delete()
 
 
-def register_library_msg_handlers(router: Router):
+def register_library_msg_handlers(router: Router) -> None:
     router.message.register(get_library, F.text == 'БИБЛИОТЕКА')
