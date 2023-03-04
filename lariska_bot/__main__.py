3376fedf67c605e96eb485a63bbc5625cf69f8be
Harlok <holmik90@yandex.ru>
2023-03-04 22:40:28 +0300

    feat(./__main__.py): add except, logger, type hinting
    register set_main_menu

--- lariska_bot/__main__.py
+++ lariska_bot/__main__.py
@@ -4,8 +4,8 @@ import os
 from dotenv import load_dotenv, find_dotenv
 
 from aiogram import Bot, Dispatcher
-from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.fsm.storage.redis import RedisStorage
 from aioredis import Redis
 
@@ -14,22 +14,30 @@ from aioredis import Redis
 from lariska_bot.handlers.message_handler import register_message_handlers
 from lariska_bot.handlers.command_handler import register_command_handler
 from lariska_bot.handlers.library_cb_handler import register_library_cb_handlers
+from lariska_bot.handlers.library_msg_handler import register_library_msg_handlers
+from lariska_bot.keyboards.set_menu import set_main_menu
 
-async def main():
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+async def main() -> None:
     logging.basicConfig(
         level='INFO',
-        format='%(asctime)s - %(levelname)s - %(name)s - %(message)s'
+        format=u'%(filename)s:%(lineno)d #%(levelname)-8s '
+               u'[%(asctime)s] - %(name)s - %(message)s'
     )
+    logger.info('is started')
+
     load_dotenv(find_dotenv())
     # bot = Bot(config.bot_token, parse_mode='HTML')
-    bot = Bot(os.getenv('BOT_TOKEN'), parse_mode='HTML')
+    bot: Bot = Bot(os.getenv('BOT_TOKEN'), parse_mode='HTML')
 
     # if config.fsm_mode == 'redis':
     #     dp = Dispatcher(storage=RedisStorage.from_url(config.redis_dsn))
     # else:
     #     dp = Dispatcher(storage=MemoryStorage())
 
-    dp = Dispatcher(storage=RedisStorage.from_url(os.getenv('REDIS_DSN')))
+    dp: Dispatcher = Dispatcher(storage=RedisStorage.from_url(os.getenv('REDIS_DSN')))
 
     # register mw
 
@@ -37,11 +45,10 @@ async def main():
     register_command_handler(dp)
     register_message_handlers(dp)
     register_library_cb_handlers(dp)
-
-    # dp.message.register(hell_reply, Text(contains=['привет'], ignore_case=True))
+    register_library_msg_handlers(dp)
 
     # register bot cmd
-
+    await set_main_menu(bot)
     # try:
     #     if not config.webhook_domain:
     #         await bot.delete_webhook(drop_pending_updates=True)
@@ -66,4 +73,7 @@ async def main():
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logger.error('stopped')
