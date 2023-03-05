19e763f6e6ffae8defdaeb94600332b8dbd586aa
Harlok <holmik90@yandex.ru>
2023-03-05 23:54:12 +0300

    feat(./__main__.py): register handlers
    register_library_cmd_handlers
    register_callback_handlers
    register_message_handlers
    add settings

--- lariska_bot/__main__.py
+++ lariska_bot/__main__.py
@@ -9,12 +9,22 @@ from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.fsm.storage.redis import RedisStorage
 from aioredis import Redis
 
+# Импорт настроек apps. Должен быть выше остальных импортов.
+from lariska_bot.settings import *
+
 # from lariska_bot.config_reader import config
 
+if library:
+    from lariska_bot.apps.library.handlers.library_cb_handler import register_library_cb_handlers
+    from lariska_bot.apps.library.handlers.library_msg_handler import register_library_msg_handlers
+    from lariska_bot.apps.library.handlers.library_cmd_handler import register_library_cmd_handlers
+
+from lariska_bot.handlers.callback_handlers import register_callback_handlers
 from lariska_bot.handlers.message_handler import register_message_handlers
 from lariska_bot.handlers.command_handler import register_command_handler
-from lariska_bot.handlers.library_cb_handler import register_library_cb_handlers
-from lariska_bot.handlers.library_msg_handler import register_library_msg_handlers
+
+from utils.db_api.db_gino import db
+
 from lariska_bot.keyboards.set_menu import set_main_menu
 
 logger: logging.Logger = logging.getLogger(__name__)
@@ -42,13 +52,29 @@ async def main() -> None:
     # register mw
 
     # register router
-    register_command_handler(dp)
+    register_callback_handlers(dp)
     register_message_handlers(dp)
-    register_library_cb_handlers(dp)
-    register_library_msg_handlers(dp)
+    register_command_handler(dp)
+
+    if library:
+        register_library_cb_handlers(dp)
+        register_library_msg_handlers(dp)
+        register_library_cmd_handlers(dp)
 
     # register bot cmd
     await set_main_menu(bot)
+
+    # postgres_url = URL.create(
+    #     "postgresql+asyncpg",
+    #     username=os.getenv("POSTGRES_USER"),
+    #     host=os.getenv('POSTGRES_HOST'),
+    #     database=os.getenv("POSTGRES_DB"),
+    #     port=int(os.getenv("POSTGRES_PORT") or 0),
+    #     password=os.getenv('POSTGRES_PASSWORD')
+    # )
+    # async_engine = create_async_engine(postgres_url)
+    # session_maker = get_session_maker(async_engine)
+
     # try:
     #     if not config.webhook_domain:
     #         await bot.delete_webhook(drop_pending_updates=True)
