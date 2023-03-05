8a93c58a5a3ee109d13af25f8e0c39e6a208995d
Harlok <holmik90@yandex.ru>
2023-03-06 00:03:32 +0300

    feat(./config_data.py): add data config

---
+++ lariska_bot/data/config_data.py
@@ -0,0 +1,12 @@
+import os
+
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+IP = str(os.getenv('IP'))
+PG_USER = str(os.getenv('PG_USER'))
+PG_PASSWORD = str(os.getenv('PG_PASSWORD'))
+DATABASE = str(os.getenv('DATABASE'))
+
+POSTGRES_URI = f'postgresql://{PG_USER}:{PG_PASSWORD}@{IP}/{DATABASE}'
