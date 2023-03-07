977ad072115baeb0398b4b6356ea92e193e377dd
Harlok <holmik90@yandex.ru>
2023-03-07 23:57:50 +0300

    fix(./data/engine.py): proceed_schemas -> deprecated
    add_trigger
    get_trigger
    get_value

--- lariska_bot/data/engine.py
+++ lariska_bot/data/engine.py
@@ -6,33 +6,20 @@ from sqlalchemy.engine.url import URL
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-#
-# def create_async_engine(url: Union[URL, str]) -> AsyncEngine:
-#     return _create_async_engine(url=url, echo=True, pool_pre_ping=True)
-#
-#
-# async def proceed_schemas(engine: AsyncEngine, metadata: MetaData) -> None:
-#     """
-#
-#     :param engine:
-#     :param metadata:
-#     :return:
-#     """
-#     # with engine.connect() as conn:
-#     #     conn.run_sync(metadata.create_all())
-#     ...
-#
-#
-# def get_session_maker(engine: AsyncEngine) -> sessionmaker:
-#     return sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+from lariska_bot.utils.deprecation_warning import deprecated
+
+
 def create_async_engine(url: Union[URL, str]) -> AsyncEngine:
     return _create_async_engine(url=url, echo=True, pool_pre_ping=True)
 
 
+@deprecated
 async def proceed_schemas(engine: AsyncEngine, metadata: MetaData) -> None:
-    async with engine.begin() as conn:
-        await conn.run_sync(metadata.create_all)
+    """deprecated."""
+    # async with engine.begin() as conn:
+    #     await conn.run_sync(metadata.create_all)
+    ...
 
 
 def get_session_maker(engine: AsyncEngine) -> sessionmaker:
-    return sessionmaker(engine, class_=AsyncSession)
+    return sessionmaker(engine, class_=AsyncSession)  # type: ignore
