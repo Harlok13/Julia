fa11aa2cb5dfea946ddf2f271358b45901ff27d6
Harlok <holmik90@yandex.ru>
2023-03-11 23:38:50 +0300

    feat(./library_filter.py): add new filters
    library_menu_filter
    book_list_filter
    book_interaction_filter
    book_choice_filter

--- lariska_bot/apps/library/filters/library_filter.py
+++ lariska_bot/apps/library/filters/library_filter.py
@@ -1,5 +1,7 @@
 from aiogram.types import CallbackQuery
 
+from lariska_bot.apps.library.lexicon.library_ikb_lexicon import MENU_IKB, BOOK_IKB
+
 
 def book_filter(callback: CallbackQuery) -> bool:
     """Фильтр книг для redis."""
@@ -7,3 +9,21 @@ def book_filter(callback: CallbackQuery) -> bool:
         return True if int(callback.data) else 'да что угодно'
     except ValueError:
         return False
+
+
+def library_menu_filter(callback: CallbackQuery) -> bool:
+    """Фильтрация callback-ов, относящихся к меню."""
+    return callback.data in list(MENU_IKB.keys())
+
+
+def book_list_filter(callback: CallbackQuery) -> bool:
+    """Фильтрация callback-ов, относящихся к списку книг"""
+    return callback.data in list(BOOK_IKB.keys())
+
+
+def book_interaction_filter(callback: CallbackQuery) -> bool:
+    return '=' in callback.data
+
+
+def book_choice_filter(callback: CallbackQuery) -> bool:
+    return callback.data.isdigit()
