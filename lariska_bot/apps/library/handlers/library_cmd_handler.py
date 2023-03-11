36e60ab45fe3ab1736a7784bd6a26324cee8f28b
Harlok <holmik90@yandex.ru>
2023-03-11 23:39:59 +0300

    ref(./library_cmd_handler.py): ref library_answer

--- lariska_bot/apps/library/handlers/library_cmd_handler.py
+++ lariska_bot/apps/library/handlers/library_cmd_handler.py
@@ -3,6 +3,7 @@ from aiogram.filters import Command
 from aiogram.types import Message
 
 from lariska_bot.apps.library.keyboards.lybrary_inline_kb import CAT_CHOICE_MENU
+from lariska_bot.apps.library.keyboards.test_ikb import main_menu
 from lariska_bot.apps.library.lexicon.library_cmd_lexicon import get_library
 from lariska_bot.apps.library.lexicon.library_menu_lexicon import MENU_LEXICON
 
@@ -10,8 +11,8 @@ from lariska_bot.apps.library.lexicon.library_menu_lexicon import MENU_LEXICON
 async def library_answer(message: Message) -> None:
     """Открыть меню библиотеки."""
     await message.delete()
-    await message.answer(MENU_LEXICON['main_menu'],
-                         reply_markup=CAT_CHOICE_MENU)
+    await message.answer(MENU_LEXICON['cat_choice_menu'],
+                         reply_markup=main_menu('cat_choice_menu'))
 
 
 def register_library_cmd_handlers(router: Router) -> None:
