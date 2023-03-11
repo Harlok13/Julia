d520d039a82a7d4f7b9be55c11493cafb4c417b3
Harlok <holmik90@yandex.ru>
2023-03-11 23:26:40 +0300

    fix(./schemas/user.py): add autoincrement, rename field

--- lariska_bot/data/schemas/user.py
+++ lariska_bot/data/schemas/user.py
@@ -9,14 +9,14 @@ class User(BaseModel):
     __tablename__ = 'users'
 
     # tlg user_id
-    user_id: Column = Column(BigInteger, unique=True, nullable=False, primary_key=True)
+    user_id: Column = Column(BigInteger, unique=True, autoincrement=True, nullable=False, primary_key=True)
     # tlg username
     username: Column = Column(VARCHAR(32), unique=True, nullable=True)
     # name of user
-    name: Column = Column(VARCHAR(32), unique=False, nullable=False)
+    user_nickname: Column = Column(VARCHAR(32), unique=False, nullable=False)
     admin: Column = Column(Boolean, default=False)
-    register_date: Column = Column(DATE, default=datetime.date.today())
-    update_date: Column = Column(DATE, onupdate=datetime.date.today())
+    register_date: Column = Column(DATE, default=datetime.date.today)
+    update_date: Column = Column(DATE, onupdate=datetime.datetime.utcnow)
 
     def __str__(self) -> str:
         return f'<User: {self.user_id}>'
