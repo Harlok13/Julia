7d70185e2d4c62f3800c7a72a2febc5015da9408
Harlok <holmik90@yandex.ru>
2023-03-14 23:40:04 +0300

    ref(./library_text_wrapper.py): ref text_wrapper
    add namedtuple

--- julia_bot/apps/library/utils/library_textwrapper.py
+++ julia_bot/apps/library/utils/library_textwrapper.py
@@ -1,12 +1,24 @@
-from typing import Union, Dict
+from collections import namedtuple
+from typing import Dict
 
 
-# ref
-def text_wrapper2(text: str, start: int, width: int):
-    """Разделить текст на страницы."""
-    symbols = (',.!?:;')
-    res = max(text[start: width + start].rfind(i) for i in symbols)
-    return res + 1, text[start: res + start + 1]
+def _text_wrapper(text: str, start: int, page_size: int) -> 'PageText':  # type: ignore
+    """
+    Split text into pages.
+
+    :param text: text to be paginated
+    :param start: index from which to start dividing per page
+    :param page_size: maximum page size
+    :return: a named tuple that stores the index of the last
+     character of the page and the text of the page itself
+    """
+    symbols: str = ',.!?:;'
+    end_of_page: int = max(text[start: page_size + start].rfind(symbol)
+                           for symbol in symbols)
+    PageText: namedtuple = namedtuple('PageText', ['page', 'text'])
+    page_with_text: PageText = PageText(page=end_of_page + 1,
+                                        text=text[start: end_of_page + start + 1])
+    return page_with_text
 
 
 # ref
