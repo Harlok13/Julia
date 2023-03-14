72502d86394c47b587068aaa5978f0aa31e057c5
Harlok <holmik90@yandex.ru>
2023-03-14 23:04:13 +0300

    fix(./trigger_request.py): fix db_add_trigger
    del extra query

--- julia_bot/apps/trigger/utils/trigger_request.py
+++ julia_bot/apps/trigger/utils/trigger_request.py
@@ -4,28 +4,29 @@ from sqlalchemy.orm import sessionmaker
 from julia_bot.apps.trigger.utils.trigger_schemas import TriggerModel
 
 
-class Request:
+class Request:  # NoteRequest
     def __init__(self, session: sessionmaker) -> None:
         self.session: sessionmaker = session
 
-    async def db_add_trigger(self, name_trigger, value_trigger) -> None:
+    async def db_add_trigger(self, name_trigger: str, value_trigger: int) -> None:
+        """Добавить триггер."""
         async with self.session.begin():
             query = f"INSERT INTO triggers_table (name_trigger, value_trigger)" \
-                    f" VALUES ('{name_trigger}', '{value_trigger}')" \
-                    f" ON CONFLICT (name_trigger)" \
-                    f" DO UPDATE SET value_trigger=triggers_table.value_trigger || '\r\n' || excluded.value_trigger"
+                    f" VALUES ('{name_trigger}', '{value_trigger}')"
             await self.session.execute(text(query))  # type: ignore
 
     async def db_get_triggers(self) -> str:
+        """Получить список триггеров."""
         async with self.session.begin():
             result_list: ChunkedIteratorResult = await self.session.execute(  # type: ignore
                 select(TriggerModel.name_trigger)
             )
             return '\r\n'.join(f"`>{result[0]}`" for result in iter(result_list))
 
-    async def db_get_values(self, name_trigger):
+    async def db_get_values(self, name_trigger: str) -> str:
+        """Получить значение триггера."""
         async with self.session.begin():
-            result = await self.session.execute(  # type: ignore
+            result: ChunkedIteratorResult = await self.session.execute(  # type: ignore
                 select(TriggerModel.value_trigger).where(TriggerModel.name_trigger == name_trigger)
             )
             return next(result)[0]
