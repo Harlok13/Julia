dd538e185582b7183c5b34050ce75e387890bdf9
Harlok <holmik90@yandex.ru>
2023-03-14 23:37:27 +0300

    fix(./library_inline_kb.py): fix say_ty_menu
    now pagination works correctly

--- julia_bot/apps/library/keyboards/lybrary_inline_kb.py
+++ julia_bot/apps/library/keyboards/lybrary_inline_kb.py
@@ -1,26 +1,26 @@
 import random
-from pprint import pprint
-from typing import Dict, List, Tuple, Union, Optional
+from typing import Dict, List, Tuple, Union
 
-from aiogram.types import InlineKeyboardMarkup
+from aiogram.types import InlineKeyboardMarkup, CallbackQuery
 from aiogram.utils.keyboard import InlineKeyboardBuilder, InlineKeyboardButton
+from aioredis import Redis
 
 from julia_bot.apps.library.lexicon.library_ikb_lexicon import MENU_IKB, BOOK_INFO_BUT, GO_MAIN_MENU, \
-    TY_BUTTONS
+    TY_BUTTONS, PAGINATION_BUT
 
 
 def main_menu(ikb_data: str) -> InlineKeyboardMarkup:
     """Выбор категорий."""
     main_menu_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
-    menu: List[Union[Tuple[str, str], Tuple[int]]] = MENU_IKB[ikb_data]
+    menu_cat: List[Union[Tuple[str, str], Tuple[int]]] = MENU_IKB[ikb_data]
     buttons: List[InlineKeyboardButton] = [
         InlineKeyboardButton(
             text=tup[0],
             callback_data=tup[1]
-        ) for tup in menu[:-1]
+        ) for tup in menu_cat[:-1]
     ]
     main_menu_builder.add(*buttons)
-    main_menu_builder.adjust(*menu[-1])
+    main_menu_builder.adjust(*menu_cat[-1])
     return main_menu_builder.as_markup(resize_keyboard=True)
 
 
@@ -35,7 +35,6 @@ def choice_book(books_list: List[Tuple[str, int]], prev: str) -> InlineKeyboardM
         ) for title, id_book in books_list
     ]
     book_menu_builder.add(*buttons)
-    # как отрефакторить?
     book_menu_builder.row(InlineKeyboardButton(text='НАЗАД', callback_data=prev))
     book_menu_builder.adjust(1)
     return book_menu_builder.as_markup(resize_keyboard=True)
@@ -51,7 +50,6 @@ def get_book_info(book_id: str, db_book_cb: str) -> InlineKeyboardMarkup:
         ) for tup in BOOK_INFO_BUT[:-1]
     ]
     book_info_builder.add(*buttons)
-    # как отрефакторить?
     book_info_builder.row(InlineKeyboardButton(text='НАЗАД', callback_data=db_book_cb))
     book_info_builder.row(InlineKeyboardButton(text=GO_MAIN_MENU['title'], callback_data=GO_MAIN_MENU['cb_data']))
     book_info_builder.adjust(*BOOK_INFO_BUT[-1])
@@ -59,32 +57,26 @@ def get_book_info(book_id: str, db_book_cb: str) -> InlineKeyboardMarkup:
 
 
 # ref
-def say_ty_menu(answer: str, pagination=None) -> InlineKeyboardMarkup:
+def say_ty_menu(answer: str, field=None, pagination=None) -> InlineKeyboardMarkup:
     """  # , pagination: Optional[str, Dict[int, str]] = None
     Закрыть сообщение. Если текста много, то создается пагинация.
+
+    :param field:
     :param pagination: give a dictionary with text to create pagination
     :param answer: required, must be 'ty_cmd' or 'praise_cmd'. responsible for button text
     :return: a button that closes the message or, if the text is long, then
      also a button with pagination
     """
-    pprint(pagination)
     ty_menu_build: InlineKeyboardBuilder = InlineKeyboardBuilder()
     ty_button: InlineKeyboardButton = InlineKeyboardButton(
         text=random.choice(TY_BUTTONS[answer]),
         callback_data='del_cmd'  # ref
     )
     ty_menu_build.row(ty_button)
-    # если текста очень много, то создается дополнительно
-    # кнопка с пагинацией
-
-    text_but = {  #ref
-        'backward': '<<',
-        'forward': '>>'
-    }
-    if len(pagination) > 1:
-        prev = InlineKeyboardButton(text=text_but['backward'], callback_data='2')
-        next = InlineKeyboardButton(text=text_but['forward'], callback_data='2')
-        count = InlineKeyboardButton(text=f'1/{len(pagination)}', callback_data='2')
-        ty_menu_build.row(prev).add(count).add(next)
+    if pagination and len(pagination) > 1:
+        prev_page = InlineKeyboardButton(text=PAGINATION_BUT['backward'], callback_data='s999')
+        next_page = InlineKeyboardButton(text=PAGINATION_BUT['forward'], callback_data=f'{field}2')
+        count_pages = InlineKeyboardButton(text=f'1/{len(pagination)}', callback_data='s0')
+        ty_menu_build.row(prev_page).add(count_pages).add(next_page)
 
     return ty_menu_build.as_markup(resize_keyboard=True)
