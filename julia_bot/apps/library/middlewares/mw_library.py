c048c50c2c0ab011a5dcf52185f6acd680256189
Harlok <holmik90@yandex.ru>
2023-03-17 23:52:46 +0300

    ref(./mw_library.py): ref LibraryMenu

--- julia_bot/apps/library/middlewares/mw_library.py
+++ julia_bot/apps/library/middlewares/mw_library.py
@@ -3,6 +3,7 @@ from typing import Callable, Dict, Any, Awaitable
 
 from aiogram import BaseMiddleware
 from aiogram.types import CallbackQuery, Message
+from aioredis import Redis
 from sqlalchemy.orm import sessionmaker
 
 from julia_bot.apps.library.lexicon.library_ikb_lexicon import MENU_IKB, BOOK_IKB
@@ -21,25 +22,23 @@ class LibraryMenu(BaseMiddleware):
     ) -> Any:
         logger = logging.getLogger(__name__)
         session_maker: sessionmaker = data['session_maker']
-
+        data['redis']: Redis = Redis()
         # генерация категорий меню
         if event.data in list(MENU_IKB.keys()):
             try:
-                data['ikb_data'] = event.data
-                logger.info(f"{data['ikb_data']}")
-                print(data['ikb_data'], 'mw_library')
+                data['ikb_data']: str = event.data
             except AttributeError as exc:
                 logger.error(exc)
 
         # генерация меню с книгами
         elif event.data in tuple(BOOK_IKB.keys()):
             async with session_maker() as session:
-                data['request'] = BookRequest(session, event.data)
-                data['prev'] = BOOK_IKB[event.data]
+                data['request']: BookRequest = BookRequest(session, event.data)
+                data['prev']: str = BOOK_IKB[event.data]
 
         # сработает в случае, если cb data будет названием книги
         elif event.data.isdigit() or '=' in event.data:
             async with session_maker() as session:
-                data['request'] = BookRequest(session, event.data)
-                # data['about_book_mw'] = event.data
+                data['request']: BookRequest = BookRequest(session, event.data)
+
         return await handler(event, data)  # type: ignore
