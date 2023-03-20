95a2b83d343ca18f6c1362217040f86662d04687
harlok <holmik90@yandex.ru>
2023-03-21 01:16:30 +0400

    fix(./schemas/book.py): rename titles

--- julia_bot/data/schemas/book.py
+++ julia_bot/data/schemas/book.py
@@ -7,8 +7,8 @@ class Book(BaseModel):
     __tablename__ = 'books'
 
     book_id: Column = Column(Integer, unique=True, nullable=False, autoincrement=True, primary_key=True)
-    name: Column = Column(VARCHAR(200), nullable=False)
+    title: Column = Column(VARCHAR(200), nullable=False)
     description: Column = Column(TEXT, default='description')
-    grandfa_desc: Column = Column(TEXT, default='grandfather description')
+    reviews: Column = Column(TEXT, default='grandfather description')
     content: Column = Column(TEXT, default='soon')
-    detailed_content: Column = Column(TEXT, default='soon')
+    # detailed_content: Column = Column(TEXT, default='soon')
