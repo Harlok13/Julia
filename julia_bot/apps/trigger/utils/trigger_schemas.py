fa6f4154f7b4b9a49dccf87276ecad6a5f915a17
harlok <holmik90@yandex.ru>
2023-03-21 01:07:52 +0400

    feat(./trigger_schemas.py): add foreign key user_id in TriggerModel

--- julia_bot/apps/trigger/utils/trigger_schemas.py
+++ julia_bot/apps/trigger/utils/trigger_schemas.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, TEXT, Integer
+from sqlalchemy import Column, TEXT, Integer, ForeignKey, BigInteger
 
 from julia_bot.apps.trigger.utils.trigger_base import BaseModel
 
@@ -9,3 +9,4 @@ class TriggerModel(BaseModel):
     trigger_id: Column = Column(Integer, autoincrement=True, primary_key=True)
     name_trigger: Column = Column(TEXT)
     value_trigger: Column = Column(TEXT)
+    user_id = Column(BigInteger, ForeignKey('users.user_id'))
